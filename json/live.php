b1cd98796e5368f44e04a21e74dd3f568ea155f3
ouhaibo <105035626+ouhaibo1980@users.noreply.github.com>
2025-05-09 12:28:48 +0800

    Add files via upload

---
+++ json/live.php
@@ -0,0 +1,104 @@
+<?php
+//**
+require_once('../404/ou.php');
+header("refresh:0;url=https://pd.qq.com/s/adkippok5");
+$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+if (strpos($ua, 'okhttp/3.15') !== false || strpos($ua, 'okhttp/4.11.0') !== false || strpos($ua, 'okhttp/4.12.0') !== false || strpos($ua, 'okhttp/4.10.0') !== false || strpos($ua, 'okhttp/3.12') !== false || strpos($ua, 'okhttp/5.0') !== false || strpos($ua, 'Mokhttp/5.0.0-alpha.10') !== false) {
+    // 不同类型的OkHttp跳转至相同页面
+?>
+[
+   
+ 
+    {
+        "name": "线路ipv6",
+        "url": "https://xn--rpb-mf3g9f.h.nxog.top/m/tv/1/&&&https://api.dzzui.com/api/avatar"
+    },
+    
+    
+   {
+            "name": "猫TV线路",
+            "type": 0,
+            "url": "http://rihou.cc:567/gggg.nzk",
+            "playerType": 1,
+            "ua": "okhttp/3.15",
+            "epg": "https://epg.112114.eu.org/?ch={name}&date={date}",
+            "logo": "https://epg.112114.eu.org/logo/{name}.png"
+        },
+  	 
+  	            {
+            "name": "新AKTV港台",
+            "type": 0,
+            "url": "http://aktv.top/live.m3u",
+            "playerType": 2,
+            "ua": "okhttp/3.15",
+            "epg": "https://epg.112114.eu.org/?ch={name}&date={date}",
+            "logo": "https://epg.112114.eu.org/logo/{name}.png"
+        },
+   
+ 
+  {
+  "name":"晴天专线",
+  "type":0,
+  "url":"http://rihou.cc:567/qtsd",
+  "playerType":2,
+  "ua":"okhttp/3.15",
+  "epg":"https://epg.112114.eu.org/?ch={name}&date={date}",
+  "logo":"https://epg.112114.eu.org/logo/{name}.png"
+  },
+
+
+  {
+  "name":"范明电台",
+  "type":0,
+  "url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/fanmingming/live/main/radio/m3u/index.m3u",
+  "playerType":2,
+  "ua":"okhttp/3.15",
+  "epg":"https://epg.112114.eu.org/?ch={name}&date={date}",
+  "logo":"https://epg.112114.eu.org/logo/{name}.png"
+  },
+
+  {
+  "name":"斗鱼线路",
+  "type":0,
+  "url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/douyuyqk.m3u",
+  "playerType":2,
+  "ua":"okhttp/3.15",
+  "epg":"https://epg.112114.eu.org/?ch={name}&date={date}",
+  "logo":"https://epg.112114.eu.org/logo/{name}.png"
+  },
+    {
+      "name": "肥羊斗鱼",
+      "type": 0,
+      "url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/douyuyqk.m3u",
+      "epg": "http://diyp2.112114.xyz/?ch={name}&date={date}",
+      "logo": "https://diyp2.112114.xyz/logo/{name}.png"
+    },
+    {
+      "name": "肥羊虎牙",
+      "type": 0,
+      "url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/Ftindy/IPTV-URL/main/huyayqk.m3u",
+      "epg": "http://diyp2.112114.xyz/?ch={name}&date={date}",
+      "logo": "https://diyp2.112114.xyz/logo/{name}.png"
+    },
+  
+
+  {
+  "name":"MV专线",
+  "type":0,
+  "url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/lystv/short/main/影视/tvb/MTV.txt","playerType":2,
+  "ua":"okhttp/3.15",
+  "epg":"https://epg.112114.eu.org/?ch={name}&date={date}",
+  "logo":"https://epg.112114.eu.org/logo/{name}.png"
+  }
+]
+
+<?php
+    exit();
+} else {
+header("Location: ../");
+
+ exit();
+}
+?>
+
